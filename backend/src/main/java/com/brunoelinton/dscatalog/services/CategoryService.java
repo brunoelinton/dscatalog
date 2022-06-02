2e46a9462338241a01440559ff28ae2acbaf5abe
Bruno Guimarães <brunoelinton@outlook.com>
2022-06-02 20:29:13 -0300

    DTO

--- backend/src/main/java/com/brunoelinton/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/brunoelinton/dscatalog/services/CategoryService.java
@@ -1,11 +1,13 @@
 package com.brunoelinton.dscatalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.brunoelinton.dscatalog.dto.CategoryDTO;
 import com.brunoelinton.dscatalog.entities.Category;
 import com.brunoelinton.dscatalog.repositories.CategoryRepository;
 
@@ -16,7 +18,8 @@ public class CategoryService {
 	private CategoryRepository repository;
 	
 	@Transactional(readOnly = true)
-	public List<Category> findAll() {
-		return repository.findAll();
+	public List<CategoryDTO> findAll() {
+		List<Category> list = repository.findAll();
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 	}
 }
