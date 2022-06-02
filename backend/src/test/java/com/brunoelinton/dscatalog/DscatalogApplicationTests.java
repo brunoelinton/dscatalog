31df740ae1394d5fdc5cbf00173fe95e35d84cfb
Bruno Guimarães <brunoelinton@outlook.com>
2022-06-01 23:15:24 -0300

    Backend project created

---
+++ backend/src/test/java/com/brunoelinton/dscatalog/DscatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brunoelinton.dscatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
