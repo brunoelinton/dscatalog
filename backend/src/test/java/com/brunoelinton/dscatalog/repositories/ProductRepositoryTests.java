e01aa811c56b08dd99e6bd0a4579536700a13e9f
Bruno Guimarães <brunoelinton@outlook.com>
2022-06-20 11:19:11 -0300

    Test repository (findById)

--- backend/src/test/java/com/brunoelinton/dscatalog/repositories/ProductRepositoryTests.java
+++ backend/src/test/java/com/brunoelinton/dscatalog/repositories/ProductRepositoryTests.java
@@ -10,22 +10,26 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.dao.EmptyResultDataAccessException;
 
 import com.brunoelinton.dscatalog.entities.Product;
+import com.brunoelinton.dscatalog.services.exceptions.ResourceNotFoundException;
+import com.brunoelinton.dscatalog.tests.Factory;
 
 @DataJpaTest
 public class ProductRepositoryTests {
-	
+
 	@Autowired
 	private ProductRepository repository;
-	
+
 	private long existingId = 1L;
 	private long nonExistingId = 1000L;
-	
+	private long countTotalProducts;
+
 	@BeforeEach
 	void setUp() throws Exception {
 		existingId = 1L;
 		nonExistingId = 1000L;
+		countTotalProducts = 25L;
 	}
-	
+
 	@Test
 	public void deleteShouldObjectWhenIdExists() {
 		// ACT
@@ -34,13 +38,39 @@ public class ProductRepositoryTests {
 		// ASSERT
 		Assertions.assertFalse(result.isPresent());
 	}
-	
+
 	@Test
-	public void deleteShouldThrowEmptyResultDataAccessExceptionWhenIdDoesNotExist() {	
+	public void deleteShouldThrowEmptyResultDataAccessExceptionWhenIdDoesNotExist() {
 		// ASSERTION
 		// ACT
 		Assertions.assertThrows(EmptyResultDataAccessException.class, () -> {
 			repository.deleteById(nonExistingId);
 		});
+	}
+
+	@Test
+	public void saveShouldPersistWithAutoIncrementWhenIdIsNull() {
+		Product product = Factory.createProduct();
+		product.setId(null);
+		product = repository.save(product);
+
+		Assertions.assertNotNull(product.getId());
+		Assertions.assertEquals(countTotalProducts + 1, product.getId());
+	}
+
+	@Test
+	public void findByIdShouldReturnNonEmptyOptionalWhenIdExists() {
+		// ACT
+		Optional<Product> product = repository.findById(existingId);
+		// ASSERTION
+		Assertions.assertTrue(product.isPresent());
+	}
+
+	@Test
+	public void findByIdShouldReturnEmptyOptionalWhenIdDoesNotExist() {
+		// ACT
+		Optional<Product> product = repository.findById(existingId);
+		// ASSERTION
+		Assertions.assertTrue(product.isPresent());
 	}
 }
