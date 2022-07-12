b7c333a25efdb539e7367669d86dd9881946a92b
Bruno Guimarães <brunoelinton@outlook.com>
2022-07-12 15:12:30 -0300

    Backend: UserUpdateValidator

--- backend/src/main/java/com/brunoelinton/dscatalog/resources/UserResource.java
+++ backend/src/main/java/com/brunoelinton/dscatalog/resources/UserResource.java
@@ -20,6 +20,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.brunoelinton.dscatalog.dto.UserDTO;
 import com.brunoelinton.dscatalog.dto.UserInsertDTO;
+import com.brunoelinton.dscatalog.dto.UserUpdateDTO;
 import com.brunoelinton.dscatalog.services.UserService;
 
 @RestController
@@ -51,9 +52,9 @@ public class UserResource {
 	}
 	
 	@PutMapping(value = "/{id}")
-	public ResponseEntity<UserDTO> update(@PathVariable Long id, @Valid @RequestBody UserDTO dto) {
-		dto = service.update(id, dto);
-		return ResponseEntity.ok().body(dto);
+	public ResponseEntity<UserDTO> update(@PathVariable Long id, @Valid @RequestBody UserUpdateDTO dto) {
+		UserDTO newDto = service.update(id, dto);
+		return ResponseEntity.ok().body(newDto);
 	}
 	
 	@DeleteMapping(value = "/{id}")
