7f8cdc40800ccb594dd4e40055b3f2ad7cd71cb5
Bruno Guimarães <brunoelinton@outlook.com>
2022-08-23 15:10:02 -0300

    Frontweb: ProductDetails layout mobile

--- frontweb/src/pages/ProductDetails/index.tsx
+++ frontweb/src/pages/ProductDetails/index.tsx
@@ -1,6 +1,34 @@
+import { ReactComponent as ArrowIcon } from 'assets/images/arrow.svg';
+import ProductPrice from 'components/ProductPrice';
+import './styles.css';
+
 const ProductDetails = () => {
     return (
-        <h1>Product Details</h1>
+        <div className="product-details-container">
+            <div className="base-card product-details-card">
+                <div className="goback-container">
+                    <ArrowIcon />
+                    <h2>VOLTAR</h2>
+                </div>
+                <div className="row">
+                    <div className="col-xl-6">
+                        <div className="img-container">
+                            <img src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg" alt="Nome do produto" />
+                        </div>
+                        <div className="name-price-container">
+                            <h1>Nome do produto</h1>
+                            <ProductPrice price={2345.67} />
+                        </div>
+                    </div>
+                    <div className="col-xl-6">
+                        <div className="description-container">
+                            <h2>Descrição do produto</h2>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, tempore sit explicabo officiis at modi fugiat quia non totam voluptatibus tenetur magnam deleniti temporibus accusamus quam in quos, ducimus enim.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
