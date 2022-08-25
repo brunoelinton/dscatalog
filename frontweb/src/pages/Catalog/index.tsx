c954e5791aa6ba3008d29ccac9e1110022321d25
Bruno Guimarães <brunoelinton@outlook.com>
2022-08-25 18:01:43 -0300

    Frontweb: Paginated request

--- frontweb/src/pages/Catalog/index.tsx
+++ frontweb/src/pages/Catalog/index.tsx
@@ -2,32 +2,44 @@ import { Product } from 'types/product';
 import Pagination from 'components/Pagination';
 import ProductCard from 'components/ProductCard';
 import { Link } from 'react-router-dom';
-
+import { useEffect, useState } from 'react';
+import { SpringPage } from 'types/vendor/spring';
+import axios from 'axios';
+import { AxiosParams } from 'types/vendor/axios';
+import { BASE_URL } from 'util/requests';
 
 import './styles.css';
 
-const product: Product = {
-  id: 2,
-  name: 'Smart TV',
-  description:
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-  price: 2190.0,
-  imgUrl:
-    'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg',
-  date: '2020-07-14T10:00:00Z',
-  categories: [
-    {
-      id: 1,
-      name: 'Livros',
-    },
-    {
-      id: 3,
-      name: 'Computadores',
-    },
-  ],
-};
-
 const Catalog = () => {
+
+  const [page, setPage] = useState<SpringPage<Product>>();
+
+  useEffect(() => {
+    const params : AxiosParams = {
+      method: 'GET',
+      url: `${BASE_URL}/products`,
+      params: {
+        page: 0,
+        size: 12
+      },
+    }
+    axios(params)
+    .then(response => {
+      setPage(response.data);
+      console.log(page);
+    });
+  }, []);
+
+  const product = {
+      "id": 13,
+      "name": "PC Gamer Plus",
+      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      "price": 1350.0,
+      "imgUrl": "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/13-big.jpg",
+      "date": "2020-07-14T10:00:00Z",
+      "categories": []
+  }
+  
   return (
     <div className="container my-4 catalog-container">
       <div className="row catalog-title-container">
