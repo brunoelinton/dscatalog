6b7c4b9aa89d857463a40fa273f19bcf7b3c784d
brunoelinton <brunoelinton@outlook.com>
2022-11-18 17:10:01 -0300

    Frontweb: Global state with Context API

--- frontweb/src/components/Navbar/index.tsx
+++ frontweb/src/components/Navbar/index.tsx
@@ -5,36 +5,32 @@ import {
   getTokenData,
   isAuthenticated,
   removeAuthData,
-  TokenData,
 } from 'util/requests';
-import { useEffect, useState } from 'react';
+import { useContext, useEffect } from 'react';
 import history from 'util/history';
-
-type AuthData = {
-  authenticated: boolean;
-  tokenData?: TokenData;
-};
+import { AuthContext } from 'AuthContext';
 
 const Navbar = () => {
-  const [authData, setAuthData] = useState<AuthData>({ authenticated: false });
 
+  const { authContextData, setAuthContextData } = useContext(AuthContext);
+  
   useEffect(() => {
     if (isAuthenticated()) {
-      setAuthData({
+      setAuthContextData({
         authenticated: true,
         tokenData: getTokenData(),
       });
     } else {
-      setAuthData({
+      setAuthContextData({
         authenticated: false,
       });
     }
-  }, []);
+  }, [setAuthContextData]);
 
   const handleLogoutClick = (event: React.MouseEvent<HTMLAnchorElement>) => {
     event.preventDefault();
     removeAuthData();
-    setAuthData({
+    setAuthContextData({
       authenticated: false,
     });
 
@@ -78,9 +74,9 @@ const Navbar = () => {
           </ul>
         </div>
         <div className="nav-login-logout">
-          {authData.authenticated ? (
+          {authContextData.authenticated ? (
             <>
-              <span className="nav-user-name">{authData.tokenData?.user_name}</span>
+              <span className="nav-user-name">{authContextData.tokenData?.user_name}</span>
               <a href="#logout" onClick={handleLogoutClick}>
                 LOGOUT
               </a>
--- frontweb/src/pages/Admin/Auth/Login/index.tsx
+++ frontweb/src/pages/Admin/Auth/Login/index.tsx
@@ -1,8 +1,9 @@
 import { Link, useHistory } from 'react-router-dom';
 import ButtonIcon from 'components/ButtonIcon';
 import { useForm } from 'react-hook-form';
-import { getAuthData, requestBackendLogin, saveAuthData } from 'util/requests';
-import { useState } from 'react';
+import { getTokenData, requestBackendLogin, saveAuthData } from 'util/requests';
+import { useContext, useState } from 'react';
+import { AuthContext } from 'AuthContext';
 
 import './styles.css';
 
@@ -12,6 +13,9 @@ type FormData = {
 };
 
 const Login = () => {
+
+  const { setAuthContextData } = useContext(AuthContext);
+
   const {
     register,
     handleSubmit,
@@ -25,10 +29,11 @@ const Login = () => {
     requestBackendLogin(formData)
       .then((response) => {
         saveAuthData(response.data);
-        const token = getAuthData().access_token;
-        console.log("Token gerado: " + token);
         setHasError(false);
-        console.log('Sucesso, ', response);
+        setAuthContextData({
+          authenticated: true,
+          tokenData: getTokenData(),
+        })
         history.push("/admin");
       })
       .catch((error) => {
