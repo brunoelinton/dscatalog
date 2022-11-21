4aa537bac9ccb571fa01e5d8765a89959a3975f9
brunoelinton <brunoelinton@outlook.com>
2022-11-21 14:11:14 -0300

    Frontweb: Organize utils

--- frontweb/src/util/requests.ts
+++ frontweb/src/util/requests.ts
@@ -1,28 +1,10 @@
 import axios, { AxiosRequestConfig } from 'axios';
 import qs from 'qs';
 import history from './history';
-import jwtDecode from 'jwt-decode';
-
-export type Role = 'ROLE_OPERATOR' | 'ROLE_ADMIN';
-
-export type TokenData = {
-  exp: number;
-  user_name: string;
-  authorities: Role[];
-}
-
-type LoginResponse = {
-  access_token: string;
-  token_type: string;
-  expires_in: number;
-  scope: string;
-  userFirstName: string;
-  userId: number;
-};
+import { getAuthData } from './storage';
 
 export const BASE_URL =
   process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
-const tokenkey = 'authData';
 
 const CLIENT_ID = process.env.REACT_APP_CLIENT_ID ?? 'dscatalog';
 const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET ?? 'dscatalog123';
@@ -65,69 +47,25 @@ export const requestBackend = (config: AxiosRequestConfig) => {
   return axios({ ...config, baseURL: BASE_URL, headers });
 };
 
-export const saveAuthData = (obj: LoginResponse) => {
-  localStorage.setItem('authData', JSON.stringify(obj));
-};
-
-export const getAuthData = () => {
-  const str = localStorage.getItem(tokenkey) ?? '{}';
-  return JSON.parse(str) as LoginResponse;
-};
-
-export const removeAuthData = () => {
-  localStorage.removeItem(tokenkey);
-}
-
 // Add a request interceptor
-axios.interceptors.request.use(function (config) {
-  return config;
-}, function (error) {
-  return Promise.reject(error);
-});
+axios.interceptors.request.use(
+  function (config) {
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
 
 // Add a response interceptor
-axios.interceptors.response.use(function (response) {
-  return response;
-}, function (error) {
-  if (error.response.status === 401) {
-    history.push("/admin/auth");
+axios.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+  function (error) {
+    if (error.response.status === 401) {
+      history.push('/admin/auth');
+    }
+    return Promise.reject(error);
   }
-  return Promise.reject(error);
-});
-
-export const getTokenData = () : TokenData | undefined => {
-  try {
-    return jwtDecode(getAuthData().access_token) as TokenData;
-  }
-  catch (error) {
-    return undefined;
-  }
-}
-
-export const isAuthenticated = () : boolean => {
-  const tokenData = getTokenData();
-
-  return (tokenData && tokenData.exp * 1000 > Date.now()) ? true : false;
-}
-
-export const hasAnyRoles = (roles: Role[]): boolean => {
-  if (roles.length === 0) {
-    return true;
-  }
-
-  const tokenData = getTokenData();
-
-  if (tokenData!== undefined) {
-    return roles.some(role => tokenData.authorities.includes(role));
-  }
-  
-  // if (tokenData!== undefined) {
-  //   for (var i = 0; i < roles.length; i++) {
-  //     if (tokenData.authorities.includes(roles[i])) {
-  //       return true;
-  //     }
-  //   }
-  // }
-
-  return false;
-}
+);
